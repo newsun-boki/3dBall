eb980ea2d2a9206549e9d49007b8f1080b9e3621
newsun-boki <1178303094@qq.com>
2021-11-24 22:52:19 +0800

    add acceleration is hard

--- src/random_ball/scripts/simu.py
+++ src/random_ball/scripts/simu.py
@@ -4,19 +4,22 @@ import cv2
 import random as rd
 import rospy
 from geometry_msgs.msg import Quaternion,PoseStamped,PointStamped
-height= 1080
-width = 720
 
 class Ball3D:
     def __init__(self):
-        self.v = np.array([0,0,0],dtype=np.int64) #定义球的速度
+        
         self.v_now = np.array([0,0,0],dtype=np.int64) #定义球的速度
         self.alpha = 0.2#速度系数
-        self.p = np.array([5,5,0])
+        self.v = np.array([0,0,0],dtype=np.int64) #定义球的速度
+        self.p = np.array([5,5,0])#位置
+        self.a = np.array([0,0,0])#加速度
+        self.a_now = np.array([0,0,0])
+
         l = []
         for i in range(8000):#定义惯性系数
             l.append([rd.random(),rd.random(),rd.random()])
         self.past_vs = np.array(l,dtype=np.int64)
+        self.past_as = np.array(l,dtype=np.int64)
         
     def update_v(self):
         self.v_now[0] = (rd.random()-0.5) * 10#通过改变这三个系数改变它
@@ -33,6 +36,22 @@ class Ball3D:
                 self.past_vs[:,i] = self.past_vs[:,i]*(-1)
                 self.v = self.v* (-1)
 
+    def update_a(self):
+        self.a_now[0] = (rd.random()-0.5) * 10#通过改变这三个系数改变它
+        self.a_now[1] = (rd.random()-0.5) * 10
+        self.a_now[2] = (rd.random()-0.5) * 10
+        
+        self.past_as =np.roll(self.past_as,1,axis = 0)
+        self.past_as[0] = self.a_now
+        self.a = np.mean(self.past_as,axis=0)
+        self.v = self.a*0.001 + self.v
+        cmp_tmp = self.p + self.alpha * self.v
+        for i in range(3):
+            if cmp_tmp[i] > 10 or cmp_tmp[i] < 0 :
+                self.past_as[:,i] = self.past_as[:,i]*(-1)
+                self.a = self.a* (-1)
+                self.v = self.v* (-1)
+        
     def move(self):
         self.update_v()
         self.p = self.p + self.alpha * self.v    
